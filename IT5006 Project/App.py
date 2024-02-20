c6e39a910071b4f1827ac886d6bc29f028f22bc6
MesKieran <84025183+MesKieran@users.noreply.github.com>
2024-02-21 00:26:31 +0800

    Add files via upload

---
+++ IT5006 Project/App.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="IT5006 Project", page_icon=":smiley:", layout="wide")
+
+
+df = pd.read_csv('ResaleFlatPricesBasedonApprovalDate19901999.csv')
+
+# Header
+st.subheader("Welcome to IT5006 Project")
+st.title("Data Analysis")
+st.sidebar.success("Select a page above")
+st.write("This is a simple example of a Streamlit app.")
+st.write("[Learn More](https://docs.streamlit.io/en/stable/)")
+
+
+with st.container():
+  st.write("---")
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("Exploartory Data Analysis")
+    st.write("##")
+    st.write(
+      """
+      content
+      """
+    )
+  with right_column:
+    df.head()
