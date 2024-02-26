4eb6b3ca2d95c17be0e605c67a8e48c0131d5cbb
zacherylzy <88877428+zacherylzy@users.noreply.github.com>
2024-02-27 00:39:26 +0800

    Update App.py

--- IT5006-Project/App.py
+++ IT5006-Project/App.py
@@ -482,7 +482,7 @@ if rental_resale_sltn == 'Resale':
   st.write('### _Data Analysis_')
 
   # Load GeoJSON file
-  geojson_file = "map4.json"
+  geojson_file = "IT5006-Project/map4.json"
   with open(geojson_file) as f:
       sg_geojson = json.load(f)
 
@@ -527,7 +527,7 @@ else:
     st.write('### _Data Analysis_')
     
     # Load GeoJSON file
-    geojson_file = "map4.json"
+    geojson_file = "IT5006-Project/map4.json"
     with open(geojson_file) as f:
         sg_geojson = json.load(f)
 
